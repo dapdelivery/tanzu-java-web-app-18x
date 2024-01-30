2293bb3c1bad734e73f2ec907fc79794c6ec34f4
Madhuri Dogra <mdogra@mdograFMD6M.vmware.com>
2024-01-30 11:06:45 +0530

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("org.springframework.boot") version "2.7.18"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("org.graalvm.buildtools.native") version "0.9.28"
+    id("java")
+}
+
+group = "com.vmware.tap.accelerators"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+extra["snakeyaml.version"] = "1.33"
+
+springBoot {
+	buildInfo()
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Spring
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.named<Jar>("jar") {
+    enabled = false
+}
